e4402c534b1ed4e7af75108065f8a10cdf907858
victor@victor <victor.dmaina@gmail.com>
2024-04-09 21:28:19 +0300

    add

--- ani.js
+++ ani.js
@@ -32,3 +32,18 @@ var swiper = new Swiper(".home-slider", {
             },
         loop:true,
         });
+
+var swiper = new Swiper(".anime-slider", {
+        slidesPerView:4,
+       spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+                   },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                },
+            loop:true,
+        });
