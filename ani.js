cf2f4fbadad4e37ab15f1477252929d3f14204c8
victor@victor <victor.dmaina@gmail.com>
2024-04-10 16:04:00 +0300

    add

--- ani.js
+++ ani.js
@@ -1,48 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const animeList = document.getElementById('anime-list');
+    const animeListElement = document.getElementById('anime-list');
+    const studioId = 569;
+const currentYear = new Date().getFullYear();
+const apiUrl = `https://api.jikan.moe/v4/anime?start_date=2019-01-01&end_date=${currentYear + 1}-01-01&producer=${studioId}`;
 
-    const apiUrl = 'https://api.jikan.moe/v4/anime';
 
-    fetchProducts();
+        
+    fetchAnime();
 
-    function fetchProducts() {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                displayAnime(data.data); // Access data array directly
-            })
-            .catch(error => console.error('Error fetching products:', error));
+    async function fetchAnime() {
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            displayAnime(data.data);
+        } catch (error) {
+            console.error('Error fetching anime:', error);
+        }
     }
 
     function displayAnime(animes) {
-        const animeList = document.getElementById("anime-list");
-        animeList.textContent = ''; // Clear existing products by removing all child nodes
+        animeListElement.textContent = ''; // Clear existing anime by removing all child nodes
         animes.forEach(anime => {
             const animeItem = document.createElement("div");
-
+    
             const imageElement = document.createElement('img');
             const imageUrl = anime.images.jpg.image_url;
             imageElement.src = imageUrl;
             imageElement.alt = anime.title;
             animeItem.appendChild(imageElement);
-
+    
             const titleElement = document.createElement('h2');
             titleElement.textContent = anime.title;
             animeItem.appendChild(titleElement);
-
+    
             const synopsisElement = document.createElement('p');
-            synopsisElement.textContent = anime.synopsis;
+            synopsisElement.textContent = anime.synopsis.length > 0 ? anime.synopsis.slice(0, 50) + '...' : anime.synopsis;
             animeItem.appendChild(synopsisElement);
-
-            
-
-            animeList.appendChild(animeItem);
+    
+            animeListElement.appendChild(animeItem);
         });
     }
+    fetchAnime();
 });
 
 
-
+    
 
     menu.onclick = () => {
       menu.classList.toggle("fa-time");
